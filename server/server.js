eb87f00c842fe3908a20b776e58824e3f64205ed
Chief Keef <pcross2@lion.lmu.edu>
2016-11-19 13:15:37 -0800

    Added Schemas

--- server/server.js
+++ server/server.js
@@ -2,6 +2,81 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var sampleValidation = {"login" : true}
+var crypto = ('crypto-js');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db = mongoose.connection;
+
+var userSchema = new Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	uid: {
+		type: Scema.Types.ObjectId,
+		ref: 'user'
+	},
+	carImage: {
+		type: String,
+		required: false,
+		default: 'images/car.png'
+	},
+	make: {
+		type: String,
+		required: false
+	},
+	licensePlate: {
+		type: String,
+		required: false
+	},
+	cardNumber: {
+		type: Number,
+		required: false
+	},
+	expDate: {
+		type: String,
+		required: false
+	},
+	nameOnCard: {
+		type: String,
+		required: false
+	},
+	CVV: {
+		type: Number,
+		required: false
+	}
+});
+
+var lotSchema = new Schema({
+	x : {
+		type: Number,
+		required: true
+	},
+	y : {
+		type: Number,
+		required: true
+	},
+	checkedIn: {
+		type: Boolean,
+		required: true
+	},
+	time: {
+		type: Date,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true
+	},
+	uid: {
+		type: Scema.Types.ObjectId,
+		ref: 'user'
+	}
+})
 
 app.get('/', function(req,res){
 	res.send("Hello World!");
