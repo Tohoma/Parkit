7779246d3847f242cbfa3ea98be3ef1c9c652b82
Chief Keef <pcross2@lion.lmu.edu>
2016-12-20 08:17:48 -0800

    Adding android assets

---
+++ android/Parkit/app/src/main/java/co/park_it/parkit/ParkitResponse.java
@@ -0,0 +1,23 @@
+package co.park_it.parkit;
+
+import com.loopj.android.http.*;
+/**
+ * Created by pcross on 11/20/16.
+ */
+
+public class ParkitResponse {
+    private static final String BASE_URL = "http://45.55.202.25:3000/";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
