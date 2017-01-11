1f31daa6890b2b3ee1481a88cd5cbba2b4a98d4d
Chief Keef <pcross2@lion.lmu.edu>
2017-01-10 22:45:11 -0800

    Added tests for registration

--- server/models/parker.js
+++ server/models/parker.js
@@ -1,11 +1,12 @@
 var db = require('./db');
 
+//TODO hash password.
+
+
 exports.create = function(parker, callback) {
 	db.open();
 	db.query('INSERT INTO parkers SET ?', parker, function(err,res){
-		//db.end(db.close);
 		if(err) callback(err);
-		console.log(res);
 		callback(null,res.insertID);
 	});
 };
@@ -13,20 +14,14 @@ exports.create = function(parker, callback) {
 exports.delete = function(username, callback){
 	db.open();
 	db.query('DELETE FROM parkers WHERE username = ?', username, function(err,res) {
-			db.end(db.close);
+			//db.end(db.close);
 			if (err) callback(err);
 			callback(null, res.affectedRows);
 			
 	})
 };
 
-// db.open();
-// db.query('SELECT * FROM parkers WHERE username = "Sample"', function(err,res) {
-// 	console.log(typeof res);
-// 	console.log(res);
-// 	console.log(res.constructor);
-// 	console.log(res === []);
-// 	db.end(db.close);
-// })
+
+
 
 
--- server/test/parkingtest.js
+++ server/test/parkingtest.js
@@ -1,5 +1,7 @@
 var parker = require('../models/parker');
 var user1 = {username:"Sample",password:"123"}
+var user2 = {username:"Random", password:"456"}
+var user3 = {username:"Fulcrum", password:"789"}
 describe('Parker', function (){
 	describe('create', function() {
 		it('should register account in database', function(done) {
@@ -9,6 +11,12 @@ describe('Parker', function (){
 			});
 
 		});
+		it('should not create account with the same username', function(done) {
+			parker.create(user1, function(err) {
+				if(err) done();
+				else done('Duplicate account created!');
+			})
+		})
 	});
 	describe('delete', function() {
 		it('should delete user account in database', function(done) {
@@ -23,6 +31,14 @@ describe('Parker', function (){
 			parker.search(user1.username, function(err,res) {
 				assert(res.length === 0);
 			})
+		});
+		it('should find user2 in database', function(done) {
+			parker.create(user2, function(err,res) {
+				parker.search(user2.username, function(err,res){
+					assert(res[0].username === user2.username);
+				})
+
+			})
 		})
-	})
+	});
 });
