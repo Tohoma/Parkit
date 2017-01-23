8fa98915b423b4f6354234544e7143e45d9c58cd
Chief Keef <pcross2@lion.lmu.edu>
2017-01-23 14:52:29 -0800

    Added Documents and modified PPnt

--- server/controllers/parker.js
+++ server/controllers/parker.js
@@ -12,14 +12,15 @@ router.post('/register', function(req, res) {
         lastname: req.body.lastname
     }
     Parker.create(parker, function(err, id) {
-        res.redirect('/');
+    	if (err) {res.status(500).json({error: true})
+    } else { res.status(200).json({error: false})}
     })
 });
 
 router.post('/login', function(req, res) {
     let username = req.body.username,
         password = req.body.password;
-    Parker.authenticate(username, password, (err, result) => res.status(400).json({result: result}))
+    Parker.authenticate(username, password, (err, result) => res.status(200).json({result: result}))
 })
 
 router.get('/', function(req,res) {
